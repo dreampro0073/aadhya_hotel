79e5fd3b348e8640bba05a4dc0734aed97bf9602
dipanshu <dipanshuchauhan23@gmail.com>
2026-02-19 22:34:25 +0530

    hotel reservertion changes

--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Hotel Reservation | New Nabratana Pvt Hospitality Hotels</title>
+
+	<meta name="description" content="Book your stay with New Nabratana Pvt Hospitality Hotels in Gorakhpur. Enjoy comfortable rooms, quality service and a pleasant stay with easy and secure hotel reservation.">
+
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<?php wp_head(); ?>
